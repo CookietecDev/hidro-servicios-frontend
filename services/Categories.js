257f9d3ccb87bf9467160a4e7b0331a04bc5aafe
unknown <gerardoquispe65@gmail.com>
2022-02-22 12:17:42 -0500

    representations

--- services/Categories.js
+++ services/Categories.js
@@ -11,7 +11,7 @@ export const CategoriesService = {
                     return res;
                 })
                 .catch((ex) => {
-                    return {};
+                    return [];
                 });
         } catch (err) {
             // console.log(err);
--- services/Representations.js
+++ services/Representations.js
@@ -11,7 +11,7 @@ export const RepresentationsService = {
                     return res;
                 })
                 .catch((ex) => {
-                    return {};
+                    return [];
                 });
         } catch (err) {
             // console.log(err);

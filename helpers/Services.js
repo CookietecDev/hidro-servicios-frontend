08933db32bef94cb9e2be8e1894b5321c19bbf7d
unknown <gerardoquispe65@gmail.com>
2022-02-22 12:10:31 -0500

    *

--- helpers/Services.js
+++ helpers/Services.js
@@ -1,5 +1,5 @@
 export const getServices = (data) => {
-  const services = data.map((item) => ({
+  const services = data?.map((item) => ({
     description: item.descripcion,
     image: `${process.env.NEXT_PUBLIC_URL_STRAPI}${item.imagen.url}`,
     name: item.nombre,
